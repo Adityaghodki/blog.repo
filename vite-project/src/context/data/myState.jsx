74510603443762f8e027b237ac5e61bc10680d69
Adityaghodki <165500746+Adityaghodki@users.noreply.github.com>
2024-11-05 22:10:59 +0530

    Update myState.jsx

--- vite-project/src/context/data/myState.jsx
+++ vite-project/src/context/data/myState.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 import MyContext from './myContext';
+import React, { useState, useEffect } from 'react';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+import { fireDB } from './firebase';  // Adjust this path to where your Firebase instance is located
+
+
 
 function MyState(props) {
     const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -15,8 +20,38 @@ function MyState(props) {
     }
     const[searchkey, setSearchkey] = useState('')
     const[loading, setloading] = useState(false);
+
+    const [getAllBlog, setGetAllBlog] = useState([]);
+
+    function getAllBlogs() {
+    setloading(true);
+    try {
+        const q = query(
+            collection(fireDB, "blogPost"),
+            orderBy('time')
+        );
+        const data = onSnapshot(q, (QuerySnapshot) => {
+            let blogArray = [];
+            QuerySnapshot.forEach((doc) => {
+                blogArray.push({ ...doc.data(), id: doc.id });
+            });
+            
+            setGetAllBlog(blogArray)
+            // console.log(productsArray)   
+                     setloading(false)
+
+        });
+        return () => data;
+    } catch (error) {
+        console.log(error)
+        setloading(false)
+    }
+}
+    useEffect(() => {
+        getAllBlogs();
+    }, []);
     return (
-        <MyContext.Provider value={{ mode, toggleMode }}>
+        <MyContext.Provider value={{ mode, toggleMode, searchkey, setSearchkey, loading, setloading, getAllBlog }}>
             {props.children}
         </MyContext.Provider>
     )
