22af465b92102ffe7eb91b04d733aa05191f205b
Adityaghodki <165500746+Adityaghodki@users.noreply.github.com>
2024-11-06 00:27:15 +0530

    Update myState.jsx

--- vite-project/src/context/data/myState.jsx
+++ vite-project/src/context/data/myState.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MyContext from './myContext';
-import React, { useState, useEffect } from 'react';
-import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
-import { fireDB } from './firebase';  // Adjust this path to where your Firebase instance is located
-
-
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { fireDb } from '../../firebase/FirebaseConfig';
+import toast from 'react-hot-toast';
 
 function MyState(props) {
     const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -18,40 +16,64 @@ function MyState(props) {
             document.body.style.backgroundColor = 'white';
         }
     }
-    const[searchkey, setSearchkey] = useState('')
-    const[loading, setloading] = useState(false);
+
+    const [searchkey, setSearchkey] = useState('')
+    const [loading, setloading] = useState(false);
 
     const [getAllBlog, setGetAllBlog] = useState([]);
 
-    function getAllBlogs() {
-    setloading(true);
-    try {
-        const q = query(
-            collection(fireDB, "blogPost"),
-            orderBy('time')
-        );
-        const data = onSnapshot(q, (QuerySnapshot) => {
-            let blogArray = [];
-            QuerySnapshot.forEach((doc) => {
-                blogArray.push({ ...doc.data(), id: doc.id });
-            });
-            
-            setGetAllBlog(blogArray)
-            // console.log(productsArray)   
-                     setloading(false)
 
-        });
-        return () => data;
-    } catch (error) {
-        console.log(error)
-        setloading(false)
+    function getAllBlogs() {
+        setloading(true);
+        try {
+            const q = query(
+                collection(fireDb, "blogPost"),
+                orderBy('time')
+            );
+            const data = onSnapshot(q, (QuerySnapshot) => {
+                let blogArray = [];
+                QuerySnapshot.forEach((doc) => {
+                    blogArray.push({ ...doc.data(), id: doc.id });
+                });
+                
+                setGetAllBlog(blogArray)
+                // console.log(productsArray)   
+                         setloading(false)
+    
+            });
+            return () => data;
+        } catch (error) {
+            console.log(error)
+            setloading(false)
+        }
     }
-}
+
     useEffect(() => {
-        getAllBlogs();
+        getAllBlogs()
     }, []);
+
+    // Blog Delete Function 
+    const deleteBlogs = async (id) => {
+        try {
+            await deleteDoc(doc(fireDb, "blogPost", id));
+            getAllBlogs()
+            toast.success("Blogs deleted successfully")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
     return (
-        <MyContext.Provider value={{ mode, toggleMode, searchkey, setSearchkey, loading, setloading, getAllBlog }}>
+        <MyContext.Provider value={{ 
+            mode, 
+            toggleMode,
+            searchkey,
+            setSearchkey,
+            loading,
+            setloading,
+            getAllBlog ,
+            deleteBlogs
+            }}>
             {props.children}
         </MyContext.Provider>
     )
